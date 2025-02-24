e119b7417db0eea767f8418a88e853debba4eb35
elinapihlajaoja <elinaakatariina@gmail.com>
2025-02-24 19:58:51 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,14 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug() << "Tila nyt: " << tila;
+    ui->num1->setEnabled(true);
+    ui->num2->setEnabled(false);
+    ui->result->setEnabled(false);
+    ui->add->setEnabled(false);
+    ui->sub->setEnabled(false);
+    ui->mul->setEnabled(false);
+    ui->div->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
@@ -13,27 +21,278 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_LisaaNappi_clicked()
+
+void MainWindow::on_N0_clicked()
 {
-    // 1. lue numero elementistä
-    QString str = ui->numeroNaytto->text();
-    int num = str.toInt();
-    qDebug() << "Numero" << num;
-    // 2. kasvata numeroa yhdellä
-    num++;
-    // 3. aseta numero takaisin elementtiin
-    qDebug() << "Kasvatettu numero" << num;
-    QString uusStr = QString::number(num);
-    ui->numeroNaytto->setText(uusStr);
+    QString str = ui->N0->text();
+    int n = str.toInt();
+    numberClickHandler(n);
 }
 
 
-void MainWindow::on_ResetNappi_clicked()
+void MainWindow::on_N1_clicked()
 {
-    // 1. aseta nolla elementtiin
-    qDebug() << "Numero asetettu takaisin nollaan";
-    QString backStr = QString::number(0);
-    ui->numeroNaytto->setText(backStr);
-    backStr = 0;
+    QString str = ui->N1->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N2_clicked()
+{
+    QString str = ui->N2->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N3_clicked()
+{
+    QString str = ui->N3->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N4_clicked()
+{
+    QString str = ui->N4->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N5_clicked()
+{
+    QString str = ui->N5->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N6_clicked()
+{
+    QString str = ui->N6->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N7_clicked()
+{
+    QString str = ui->N7->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N8_clicked()
+{
+    QString str = ui->N8->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+void MainWindow::on_N9_clicked()
+{
+    QString str = ui->N9->text();
+    int n = str.toInt();
+    numberClickHandler(n);
+}
+
+
+
+void MainWindow::on_add_clicked()
+{
+    // tila1: syötä numero 1
+    // tila 2: syötä numero 2
+    // tila 3: laske tulos
+    // palaa tilaan 1
+
+
+    if(tila == 3) {
+
+    QString num1Str = ui->num1->text();
+    int num1 = num1Str.toInt();
+
+    QString num2Str = ui->num2->text();
+    int num2 = num2Str.toInt();
+
+    int result = num1+num2;
+    qDebug() << "Plus:" << result << "=" << num1 << "+" << num2;
+
+    QString resStr = QString::number(result);
+    ui->result->setText(resStr);
+
+    tila = 1;
+
+    ui->num1->setEnabled(true);
+    ui->num2->setEnabled(false);
+    ui->result->setEnabled(false);
+    ui->add->setEnabled(false);
+    ui->sub->setEnabled(false);
+    ui->mul->setEnabled(false);
+    ui->div->setEnabled(false);
+
+
+    qDebug() << "Laskun jälkeen tila:" << tila;
+    } else {
+        qDebug() << "Virhe: Molempia numeroita ei syötetty";
+    }
+}
+
+void MainWindow::on_sub_clicked()
+{
+    QString num1Str = ui->num1->text();
+    int num1 = num1Str.toInt();
+
+    QString num2Str = ui->num2->text();
+    int num2 = num2Str.toInt();
+
+    int result = num1-num2;
+    qDebug() << "Miinus:" << result << "=" << num1 << "-" << num2;
+
+    QString resStr = QString::number(result);
+    ui->result->setText(resStr);
+
+    tila = 1;
+
+    ui->num1->setEnabled(true);
+    ui->num2->setEnabled(false);
+    ui->result->setEnabled(false);
+    ui->add->setEnabled(false);
+    ui->sub->setEnabled(false);
+    ui->mul->setEnabled(false);
+    ui->div->setEnabled(false);
+
+    qDebug() << "Laskun jälkeen tila:" << tila;
+}
+
+
+void MainWindow::on_mul_clicked()
+{
+    QString num1Str = ui->num1->text();
+    int num1 = num1Str.toInt();
+
+    QString num2Str = ui->num2->text();
+    int num2 = num2Str.toInt();
+
+    int result = num1*num2;
+    qDebug() << "Kerto:" << result << "=" << num1 << "*" << num2;
+
+    QString resStr = QString::number(result);
+    ui->result->setText(resStr);
+
+    tila = 1;
+
+    ui->num1->setEnabled(true);
+    ui->num2->setEnabled(false);
+    ui->result->setEnabled(false);
+    ui->add->setEnabled(false);
+    ui->sub->setEnabled(false);
+    ui->mul->setEnabled(false);
+    ui->div->setEnabled(false);
+
+
+    qDebug() << "Laskun jälkeen tila:" << tila;
+}
+
+
+void MainWindow::on_div_clicked()
+{
+    QString num1Str = ui->num1->text();
+    int num1 = num1Str.toInt();
+
+    QString num2Str = ui->num2->text();
+    int num2 = num2Str.toInt();
+
+    int result = num1/num2;
+    qDebug() << "Jako:" << result << "=" << num1 << "/" << num2;
+
+    QString resStr = QString::number(result);
+    ui->result->setText(resStr);
+
+    tila = 1;
+
+    ui->num1->setEnabled(true);
+    ui->num2->setEnabled(false);
+    ui->result->setEnabled(false);
+    ui->add->setEnabled(false);
+    ui->sub->setEnabled(false);
+    ui->mul->setEnabled(false);
+    ui->div->setEnabled(false);
+
+    qDebug() << "Laskun jälkeen tila:" << tila;
+}
+
+
+void MainWindow::numberClickHandler(int n)
+{
+    qDebug() << "Valittu numero = " << n;
+
+    QString str = QString::number(n);
+
+    if (tila == 1) {
+        qDebug() << "Elementti num1" << n;
+        //laita num1
+        ui->num1->setText(str);
+    } else if (tila == 2) {
+        qDebug() << "Elementti num2" << n;
+        // laita n -> num2
+        ui->num2->setText(str);
+    }
+}
+
+
+void MainWindow::on_enter_clicked()
+{
+    // tila1: syötä numero 1
+    // tila 2: syötä numero 2
+    // tila 3: laske tulos
+    // palaa tilaan 1
+    if (tila > 3) {
+        tila = 3;
+    } else {
+    tila++;
+    }
+
+    qDebug() << "Siirrytty tilaan" << tila;
+
+    // käyttöliittymän elementtien aktivointi
+    if (tila == 1) {
+        ui->num1->setEnabled(true);
+        ui->num2->setEnabled(false);
+        ui->result->setEnabled(false);
+        ui->add->setEnabled(false);
+        ui->sub->setEnabled(false);
+        ui->mul->setEnabled(false);
+        ui->div->setEnabled(false);
+    } else if (tila == 2) {
+        ui->num1->setEnabled(false);
+        ui->num2->setEnabled(true);
+        ui->result->setEnabled(false);
+        ui->add->setEnabled(false);
+        ui->sub->setEnabled(false);
+        ui->mul->setEnabled(false);
+        ui->div->setEnabled(false);
+    } else if (tila == 3) {
+        ui->num1->setEnabled(false);
+        ui->num2->setEnabled(false);
+        ui->result->setEnabled(true);
+        ui->add->setEnabled(true);
+        ui->sub->setEnabled(true);
+        ui->mul->setEnabled(true);
+        ui->div->setEnabled(true);
+    }
+
+}
+
+
+void MainWindow::on_clear_clicked()
+{
+    ui->num1->setText("0");
+    ui->num2->setText("0");
+    ui->result->setText("0");
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -18,11 +18,41 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_LisaaNappi_clicked();
+    void on_add_clicked();
+    void on_N1_clicked();
+    void on_N2_clicked();
+    void on_N3_clicked();
 
-    void on_ResetNappi_clicked();
+    void on_N4_clicked();
+
+    void on_N5_clicked();
+
+    void on_N6_clicked();
+
+    void on_N7_clicked();
+
+    void on_N8_clicked();
+
+    void on_N9_clicked();
+
+    void on_N0_clicked();
+
+    void on_sub_clicked();
+
+    void on_mul_clicked();
+
+    void on_div_clicked();
+
+    void on_enter_clicked();
+
+    void on_clear_clicked();
 
 private:
+    void numberClickHandler(int n);
+
     Ui::MainWindow *ui;
+
+    int tila = 1;
+
 };
 #endif // MAINWINDOW_H
